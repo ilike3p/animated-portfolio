ac5506e963122caac50558c734e9c3c034b6c17b
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-11-04 23:40:40 -0700

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,19 @@ import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <div className='navbar'>Navbar</div>
+        <div className='navbar'>
+        {/* Sidebar */}
+        <div className="wrapper">
+            <span>Tray Dev</span>
+            <div className="social">
+                <a href="https://www.facebook.com/tray.pearce"><img src="/facebook.png" alt="" /></a>
+                <a href="https://www.instagram.com/ilike3p/"><img src="/instagram.png" alt="" /></a>
+                <a href="https://www.linkedin.com/in/tray-pearce-04018a20/"><img src="/linkedin.png" alt="" /></a>
+                <a href="https://github.com/ilike3p"><img src="/github.png" alt="" /></a>
+
+            </div>
+        </div>
+        <div/>
     )
 }
 
