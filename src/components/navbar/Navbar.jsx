1a69647e8df5827dd2434d2c9206de76cd576109
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-11-06 12:52:24 -0800

    update png

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,19 +4,20 @@ import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <div className='navbar'>
-        {/* Sidebar */}
-        <div className="wrapper">
-            <span>Tray Dev</span>
-            <div className="social">
-                <a href="https://www.facebook.com/tray.pearce"><img src="/facebook.png" alt="" /></a>
-                <a href="https://www.instagram.com/ilike3p/"><img src="/instagram.png" alt="" /></a>
-                <a href="https://www.linkedin.com/in/tray-pearce-04018a20/"><img src="/linkedin.png" alt="" /></a>
-                <a href="https://www.youtube.com/channel/UCF_kBFji1u95wd_5-Dx_APw"><img src="/github.png" alt="" /></a>
-
+        <div>
+            {/* Sidebar */}
+            <div className="wrapper">
+                <span>Tray Dev</span>
+                <div className="social">
+                    <a href="https://www.facebook.com/tray.pearce"><img src="/facebook.png" alt="" /></a>
+                    <a href="https://www.instagram.com/ilike3p/"><img src="/instagram.png" alt="" /></a>
+                    <a href="https://www.linkedin.com/in/tray-pearce-04018a20/"><img src="/linkedin.png" alt="" /></a>
+                    <a href="https://www.youtube.com/channel/UCF_kBFji1u95wd_5-Dx_APw"><img src="/youtube.png" alt="" /></a>
+                </div>
             </div>
+            {/* End Sidebar */}
         </div>
-        <div/>
+    );
     )
 }
 
